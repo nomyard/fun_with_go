b9b45f95ce8e608b4d500c77d9a5fe2a54b219ec
nomyard <nomyards@gmail.com>
2018-06-03 19:28:38 -0400

    some variable practice

---
+++ variables/shorthand-type/main.go
@@ -0,0 +1,17 @@
+package main
+
+import "fmt"
+
+func main() {
+
+	a := 10
+	b := "golang"
+	c := 4.17
+	d := true
+
+	fmt.Printf("%T \n", a) //%T prints the type of the value given
+	fmt.Printf("%T \n", b)
+	fmt.Printf("%T \n", c)
+	fmt.Printf("%T \n", d)
+
+}
