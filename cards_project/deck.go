841f088737f27ab4c345c0c8d3f6198787652cfc
nomyard <nomyards@gmail.com>
2018-06-05 13:56:46 -0400

    playing with tests

--- cards_project/deck.go
+++ cards_project/deck.go
@@ -3,7 +3,10 @@ package main
 import (
 	"fmt"
 	"io/ioutil"
+	"math/rand"
+	"os"
 	"strings"
+	"time"
 )
 
 // Create a new type of 'deck'
@@ -38,17 +41,45 @@ func (d deck) print() {
 	}
 }
 
+//This function is
 func deal(d deck, handSize int) (deck, deck) {
 	return d[:handSize], d[handSize:]
 }
 
+// This function is turning the deck slice into a string
 func (d deck) toString() string {
 	return strings.Join([]string(d), ",")
 
 }
 
+//This function is writing to the hard drive
 func (d deck) saveToFile(filename string) error {
 	return ioutil.WriteFile(filename, []byte(d.toString()), 0666)
+}
+
+func newDeckFromFile(filename string) deck {
+	bs, err := ioutil.ReadFile(filename)
+	if err != nil {
+		//Option #1 - log the error and return a call to newDeck()
+		//Option #2 - log the error and entirely quit the program
+		fmt.Println("Error:", err)
+		os.Exit(1)
+	}
+
+	s := strings.Split(string(bs), ",")
+	return deck(s)
+}
+
+//go through this function line by line to understand this better!****
+func (d deck) shuffle() {
+	source := rand.NewSource(time.Now().UnixNano())
+	r := rand.New(source)
+
+	for i := range d {
+		newPosition := r.Intn(len(d) - 1)
+
+		d[i], d[newPosition] = d[newPosition], d[i]
+	}
 }
 
 // d deck
--- cards_project/main.go
+++ cards_project/main.go
@@ -2,7 +2,8 @@ package main
 
 func main() {
 	cards := newDeck()
-	cards.saveToFile("my_cards")
+	cards.shuffle()
+	cards.print()
 
 }
 
